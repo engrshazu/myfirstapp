2f2230a91c8b52fefc86f8598e49cdfb501ef361
shehzad <engineer.mshehzad@gmail.com>
2019-08-03 15:22:21 +0500

    adding new show page and flash message to the customer

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,22 @@ end
 
     #render :plain => params[:article].inspect
     @article= Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+    #do something
+    flash[:notice]="Article successfuly created"
+    redirect_to article_path(@article)
+  else
+      render 'new'
+    end
   end
+
+def show
+  @article=Article.find(params[:id])
+end
+
+
   private
-  def article_params
+def article_params
   params.require(:article).permit(:title, :description)
   end
 end
